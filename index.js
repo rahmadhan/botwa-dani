ab597bb18ae443cb04e1f8b385453b914e79d239
rahmadhan <74216640+rahmadhan@users.noreply.github.com>
2020-11-10 06:56:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'DANI BOTT :V'; 
+const instagram = 'https://instagram.com/rahmadanireal'; 
+const telegram = 'https://t.me/ini danii'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/gaadayutup:(';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
